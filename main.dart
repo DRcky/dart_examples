b034de33ad128d2a3b3198b30d2380c0fc1aa59d
DRcky <120072542+DRcky@users.noreply.github.com>
2024-10-08 11:06:17 -0400

    Feat: Created class with property, getter and setter and using it

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
+//Creating class and added getter and setter for a property
+class MyClass {
+  int _aProperty = 0;
+  int get aProperty => _aProperty;
+  set aProperty(int value) {
+    if (value >= 0) {
+      _aProperty = value;
+    }
+  }
+}
+
 void main() {
   // String interpolation
   print('${10 + 5}');
@@ -7,4 +18,11 @@ void main() {
 
   bool hasEmpty = aListOfStrings.any((s) => s.isEmpty);
   print(hasEmpty);
+
+  var myClass = MyClass();
+  //Print actual value of _aProperty
+  print(myClass._aProperty);
+  myClass._aProperty = 10;
+  //Print new value for _aProperty
+  print(myClass._aProperty);
 }
