ff0a2baec1de9ac6261eecb5c4590085ac7ade7d
DRcky <120072542+DRcky@users.noreply.github.com>
2024-10-08 10:46:50 -0400

    Feat:  arrow syntax and collections example

--- main.dart
+++ main.dart
@@ -1,4 +1,10 @@
 void main() {
   // String interpolation
   print('${10 + 5}');
+
+    // arrow syntax and collections
+  final aListOfStrings = ['one', 'two', 'three'];
+
+  bool hasEmpty = aListOfStrings.any((s) => s.isEmpty);
+  print(hasEmpty);
 }
